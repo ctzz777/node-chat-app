d7f8b3c1076fbfdbb897567d5c8c21cbcb45b8a2
CTzz <ctzz@protonmail.com>
2017-08-30 00:12:46 +0800

    Add newMessage and createMessage events

--- server/server.js
+++ server/server.js
@@ -14,10 +14,30 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
     console.log('New user connected');
 
+    socket.emit('newMessage', {
+        from: 'Teddy',
+        text: 'See you',
+        createAt: 123123
+    })
+
+    socket.emit('newEmail', {
+        from: 'test@email.com',
+        text: 'Hey!',
+        createAt: 124
+    });
+    
+    socket.on('createMessage', (message) => {
+        console.log('createMessage', message);
+    });
+
+    socket.on('createEmail', (newEmail) => {
+        console.log('createEmail', newEmail);
+    });
+
     socket.on('disconnect', () => {
         console.log('User was disconnected');
     });
-})
+});
 
 server.listen(port, () => {
     console.log(`Server is up on port ${port}`);
