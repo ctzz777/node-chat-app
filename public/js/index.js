0af99574638c064166c400072ced2a8730f3f909
CTzz <ctzz@protonmail.com>
2017-08-31 00:18:52 +0800

    Emit newMessage on createMessage

--- public/js/index.js
+++ public/js/index.js
@@ -3,10 +3,10 @@ var socket = io();
 socket.on('connect', function() {
     console.log('Connected to server');
 
-    socket.emit('createMessage', {
-        from: 'Ted',
-        text: 'Yep?'
-    })
+    // socket.emit('createMessage', {
+    //     from: 'Ted',
+    //     text: 'Yep?'
+    // })
 });
 
 socket.on('disconnect', function() {
